068ffd2b327d0d937ff0c31fe33a590781197249
ananias <netim.cunha@gmail.com>
2025-08-23 12:49:08 -0300

    fix: corrige CORS e ajusta middleware de validação para suportar upload de imagens

--- backend/src/middleware/middlewareSchema.js
+++ backend/src/middleware/middlewareSchema.js
@@ -7,7 +7,22 @@ export function validateSchema(schema) {
             res.status(422).json({ errors });
             return; 
         }
-
         next();
     };
+}
+export function validateSchemaWhitImage(schema, fileFieldName) {
+  return (req, res, next) => {
+    const data = { ...req.body };
+    if (req.file) {
+      data[fileFieldName] = req.file;
+    }
+
+    const { error } = schema.validate(data, { abortEarly: false });
+    if (error) {
+      const errors = error.details.map(detail => detail.message);
+      return res.status(422).json({ errors });
+    }
+
+    next();
+  };
 }
